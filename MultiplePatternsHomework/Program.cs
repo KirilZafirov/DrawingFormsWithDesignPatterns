24d9cba860b0c5b53c71f68da4bdc1334c879520
KZafirov <kiril_zafirov@yahoo.com>
2017-05-16 12:34:18 +0200

    TestChange

--- MultiplePatternsHomework/Program.cs
+++ MultiplePatternsHomework/Program.cs
@@ -15,18 +15,7 @@ namespace MultiplePatternsHomework
             var answer = new List<AbstractShape>();
             myApp.Action(answer);
             
-            //The following implementation is with the Implemented Iterator Pattern
-            //AbstractShape collection = new Circle();
-            //collection[0] = new Circle(10, 10, 20, Enumerations.ColorEnum.Blue, false, false, "Circle");
-            //collection[1] = new Circle(10, 10, 20, Enumerations.ColorEnum.Blue, false, false, "Circle");
-            //collection[2] = new Circle(10, 10, 20, Enumerations.ColorEnum.Blue, false, false, "Circle");
-            //collection[3] = new Circle(10, 10, 20, Enumerations.ColorEnum.Blue, false, false, "Circle");
-            //collection[4] = new Circle(10, 10, 20, Enumerations.ColorEnum.Blue, false, false, "Circle");
-            //collection[5] = new Circle(10, 10, 20, Enumerations.ColorEnum.Blue, false, false, "Circle");
-            //ListShapesWithIterator(collection);
-
-            //var c = new Circle(10, 10, 20, Enumerations.ColorEnum.Blue, false, false, "Circle");
-            //var managerCommand = new ShapeManager(c);
+          
 
             Console.WriteLine();
             Console.ReadLine();
